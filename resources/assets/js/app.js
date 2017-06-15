e982c0bbb6aa51c8bb990af35b79b881fcfbea50
Joel <jijoel@yahoo.com>
2017-06-15 20:31:39 +0000

    include vuex

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,6 +9,11 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import Vuex from 'vuex'
+Vue.use(Vuex);
+
+require('vuetify');
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
