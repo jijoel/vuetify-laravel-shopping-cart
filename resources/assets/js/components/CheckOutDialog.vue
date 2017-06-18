a2f9fd8a13864395708e9617e89aaaa07f73f301
Joel <jijoel@yahoo.com>
2017-06-18 16:34:52 +0000

    show prices in checkout dialog

--- resources/assets/js/components/CheckOutDialog.vue
+++ resources/assets/js/components/CheckOutDialog.vue
@@ -112,6 +112,31 @@
           </v-stepper-step>
           <v-stepper-content step="3">
 
+          <div class="table__overflow">
+            <table class="datatable table">
+              <tbody>
+                <tr>
+                  <td>Subtotal ({{ item_count }} items):</td>
+                  <td class="text-xs-right">{{ subtotal | money }}</td>
+                </tr>
+                <tr>
+                  <td>Shipping:</td>
+                  <td class="text-xs-right">{{ shipping | money }}</td>
+                </tr>
+                <tr>
+                  <td>Taxes:</td>
+                  <td class="text-xs-right">{{ taxes | money }}</td>
+                </tr>
+                <tr>
+                  <td><strong>Grand Total:</strong></td>
+                  <td class="text-xs-right"><strong>{{ grand_total | money }}</strong></td>
+                </tr>
+
+              </tbody>
+            </table>
+          </div><!-- .table__overflow -->
+
+
             <p>
               Please enter your credit card information
               below:
@@ -189,6 +214,14 @@ export default {
         ? 'v-dialog-bottom-transition'
         : 'v-dialog-transition';
     },
+
+    items() { return this.$store.getters.cartItems },
+    item_count() { return this.$store.getters.cartItemCount },
+    subtotal() { return this.$store.getters.cartSubtotal },
+    shipping() { return this.$store.getters.cartShipping },
+    taxes() { return this.$store.getters.cartTaxes },
+    grand_total() { return this.$store.getters.cartGrandTotal },
+
   },
 
   methods: {
