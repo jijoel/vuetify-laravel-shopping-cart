707337adcce37c77f4337a7a0c1cc64f181be63f
Joel <jijoel@yahoo.com>
2017-06-18 16:42:23 +0000

    refactor to move totals to own object

--- resources/assets/js/components/CheckOutDialog.vue
+++ resources/assets/js/components/CheckOutDialog.vue
@@ -112,30 +112,7 @@
           </v-stepper-step>
           <v-stepper-content step="3">
 
-          <div class="table__overflow">
-            <table class="datatable table">
-              <tbody>
-                <tr>
-                  <td>Subtotal ({{ item_count }} items):</td>
-                  <td class="text-xs-right">{{ subtotal | money }}</td>
-                </tr>
-                <tr>
-                  <td>Shipping:</td>
-                  <td class="text-xs-right">{{ shipping | money }}</td>
-                </tr>
-                <tr>
-                  <td>Taxes:</td>
-                  <td class="text-xs-right">{{ taxes | money }}</td>
-                </tr>
-                <tr>
-                  <td><strong>Grand Total:</strong></td>
-                  <td class="text-xs-right"><strong>{{ grand_total | money }}</strong></td>
-                </tr>
-
-              </tbody>
-            </table>
-          </div><!-- .table__overflow -->
-
+            <s-shopping-cart-totals></s-shopping-cart-totals>
 
             <p>
               Please enter your credit card information
@@ -214,14 +191,6 @@ export default {
         ? 'v-dialog-bottom-transition'
         : 'v-dialog-transition';
     },
-
-    items() { return this.$store.getters.cartItems },
-    item_count() { return this.$store.getters.cartItemCount },
-    subtotal() { return this.$store.getters.cartSubtotal },
-    shipping() { return this.$store.getters.cartShipping },
-    taxes() { return this.$store.getters.cartTaxes },
-    grand_total() { return this.$store.getters.cartGrandTotal },
-
   },
 
   methods: {
@@ -238,6 +207,12 @@ export default {
     },
 
   },
+
+  watch: {
+    value() {
+      this.value && this.loadPaymentProcessor();
+    },
+  }
 
 
 }
